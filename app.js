d41cc6d508bc3c95fca999dc1f7223100fed6021
Juninho <juninhopeter1@gmail.com>
2022-05-02 22:18:15 +0100

    added new features

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
+// querying the DOM
 const loanFoarm = document.querySelector('form');
 const hisDiv = document.querySelector('.histy');
 const resetButton = document.querySelector('.btFresh');
 
-
+// Adding evenlistener to the form
 loanFoarm.addEventListener('submit', (juninho) =>{
     juninho.preventDefault();
     
@@ -15,6 +16,8 @@ loanFoarm.addEventListener('submit', (juninho) =>{
         totalCalc = (1 + this.percentage);
         allCalc = Math.pow(this.totalCalc, this.yearNum);
         
+
+        // initializing the class
         constructor () {
             
             this.amount;
@@ -24,14 +27,16 @@ loanFoarm.addEventListener('submit', (juninho) =>{
             this.totalCalc;
             
             
-            
+            // multiplying the amount and the totalCalc together
             let totalAmount = (this.amount * this.allCalc).toLocaleString('en');
             let newAmount = this.amount.toLocaleString('en');
             
+            // showing the result and summary
             if (totalAmount) {
                 document.getElementById('result').textContent = `₦${totalAmount}`;
                 document.getElementById('notice').textContent = `You're expected to pay the total sum of ${totalAmount} in ${this.yearNum} year(s)`;
 
+                // result history at the top of the result
                 setTimeout(() => {
                     document.getElementById('displ1').textContent = `${newAmount}`;
                     document.getElementById('displ2').textContent = `${loanFoarm.perIn.value}%`;
@@ -41,6 +46,9 @@ loanFoarm.addEventListener('submit', (juninho) =>{
                 
             };
             
+            
+            // when the result is not a number, the result field should be blank
+
             if (isNaN(this.amount) || isNaN(this.percentage) || isNaN(this.yearNum)) {
                 document.getElementById('result').textContent = '';
                 
@@ -50,8 +58,10 @@ loanFoarm.addEventListener('submit', (juninho) =>{
     }
     new Rate()
     
+    // resetting the result and form
     resetButton.addEventListener('click', (jay) => {
         loanFoarm.reset();
+
     })
 })
 
